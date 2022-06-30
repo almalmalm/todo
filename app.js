cde0cce5b44874ba32a3529bcd551386f970ae71
almalmalm <almostkalinin@gmail.com>
2022-06-30 22:07:00 +0300

    Rewrite add button function

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 // User enters text in input field
+
 const userInput = document.getElementById('user-input');
 
 // When user click on 'Add TODO' button, the text adds to todo list
-const addButton = document.getElementById('add-button');
+const addBtn = document.getElementById('add-button');
 
-addButton.onclick = createListItem;
-
-// возвращает null, понять почему
-const doneButton = document.getElementById('done-button');
-const deleteButton = document.getElementById('delete-button');
-
-deleteButton.onclick = deleteListItem;
-
-function createListItem (event) {
+addBtn.addEventListener('click', function () {
     const newDiv = document.createElement('div');
     const doneButton = document.createElement('button');
     const deleteButton = document.createElement('button');
@@ -26,10 +19,27 @@ function createListItem (event) {
     divList.appendChild(newDiv);
     newDiv.appendChild(doneButton);
     newDiv.appendChild(deleteButton);
-}
+});
 
-function deleteListItem (event) {
-    const realDiv = document.getElementById('list-item');
-    realDiv.remove();
-    alert('removeEvent');
-}
+const deleteBtn = document.getElementById('delete-button');
+
+deleteBtn.addEventListener('click', function () {
+    console.log('delete');
+});
+
+
+// function createListItem (event) {
+//     const newDiv = document.createElement('div');
+//     const doneButton = document.createElement('button');
+//     const deleteButton = document.createElement('button');
+//     newDiv.innerHTML = userInput.value;
+//     newDiv.setAttribute('id','list-item');
+//     doneButton.innerText = 'Done';
+//     doneButton.setAttribute('id', 'done-button');
+//     deleteButton.innerText = 'Delete';
+//     deleteButton.setAttribute('id', 'delete-button')
+//     const divList = document.getElementById('list');
+//     divList.appendChild(newDiv);
+//     newDiv.appendChild(doneButton);
+//     newDiv.appendChild(deleteButton);
+// }
