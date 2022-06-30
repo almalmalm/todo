c1dc0587ca62bf29b0edfb0e05bacca881013960
almalmalm <almostkalinin@gmail.com>
2022-06-30 23:58:29 +0300

    Add Done button functions

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ function addItemList () {
     const newDiv = document.createElement('div');
     const doneButton = document.createElement('button');
     const deleteButton = document.createElement('button');
-    newDiv.innerHTML = userInput.value;
+    //newDiv.innerHTML = userInput.value;
+    const text = userInput.value;
+    const textDiv = document.createElement('div');
+    textDiv.innerHTML = text;
+    textDiv.setAttribute('id', 'text');
     newDiv.setAttribute('id','list-item');
     doneButton.innerText = 'Done';
     doneButton.setAttribute('id', 'done-button');
@@ -19,31 +23,28 @@ function addItemList () {
     deleteButton.setAttribute('id', 'delete-button')
     const divList = document.getElementById('list');
     divList.appendChild(newDiv);
+    console.log(text + ' <- text is');
+    newDiv.appendChild(textDiv);
     newDiv.appendChild(doneButton);
     newDiv.appendChild(deleteButton);
     console.log('Added list item');
+
+    const deleteBtn = document.getElementById('delete-button');
+
+    deleteBtn.addEventListener('click', deleteItemList);
+
+    function deleteItemList () {
+        console.log('delete');
+        divList.removeChild(newDiv);
+    }
+
+    const doneBtn = document.getElementById('done-button');
+
+    doneBtn.addEventListener('click', doneItemList);
+
+    function doneItemList () {
+        console.log('done');
+        const item = document.querySelector('#text');
+        item.classList.add('strike');
+    }
 }
-
-const deleteBtn = document.getElementById('delete-button');
-
-deleteBtn.addEventListener('click', deleteItemList);
-
-function deleteItemList () {
-    console.log('delete');
-}
-
-// function createListItem (event) {
-//     const newDiv = document.createElement('div');
-//     const doneButton = document.createElement('button');
-//     const deleteButton = document.createElement('button');
-//     newDiv.innerHTML = userInput.value;
-//     newDiv.setAttribute('id','list-item');
-//     doneButton.innerText = 'Done';
-//     doneButton.setAttribute('id', 'done-button');
-//     deleteButton.innerText = 'Delete';
-//     deleteButton.setAttribute('id', 'delete-button')
-//     const divList = document.getElementById('list');
-//     divList.appendChild(newDiv);
-//     newDiv.appendChild(doneButton);
-//     newDiv.appendChild(deleteButton);
-// }
